dc9e8557d8361a40c83ba6c29e5a48c29e49ff0b
CoffeeKnyte <67730400+CoffeeKnyte@users.noreply.github.com>
2020-11-30 11:54:23 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,25 @@
 #bin/bash
 
-ask client ID/secret
-add rclone config
+sudo mkdir -p /mnt/zenstorage
+sudo mkdir -p /mnt/zenremote
 
-sudo wget vfs files
+sudo wget https://raw.githubusercontent.com/CoffeeKnyte/local-remote/main/service/zenremote.service -P /etc/systemd/system/
+sudo wget https://raw.githubusercontent.com/CoffeeKnyte/local-remote/main/service/zenremote_primer.service -P /etc/systemd/system/
+sudo wget https://raw.githubusercontent.com/CoffeeKnyte/local-remote/main/service/zenremote_primer.timer -P /etc/systemd/system/
 
-daemon reload
-primer
-mergerfs reload
-restart all dockers
+sudo wget https://raw.githubusercontent.com/CoffeeKnyte/local-remote/main/service/zenstorage.service -P /etc/systemd/system/
+sudo wget https://raw.githubusercontent.com/CoffeeKnyte/local-remote/main/service/zenstorage_primer.service -P /etc/systemd/system/
+sudo wget https://raw.githubusercontent.com/CoffeeKnyte/local-remote/main/service/zenstorage_primer.timer -P /etc/systemd/system/
+
+sudo rm  /etc/systemd/system/mergerfs.service
+
+sudo wget https://raw.githubusercontent.com/CoffeeKnyte/local-remote/main/service/mergerfs.service -P /etc/systemd/system/
+
+sudo systemctl daemon-reload
+sudo systemctl enable zenstorage.service && sudo systemctl enable zenstorage_primer.service
+sleep 10
+sudo systemctl enable zenremote.service && sudo systemctl enable zenremote_primer.service
+sleep 10
+sudo systemctl restart zenstorage.service && sudo systemctl restart zenstorage_primer.service && sudo systemctl restart zenremote.service && sudo systemctl restart zenremote_primer.service && sudo systemctl restart mergerfs.service
+
+#restart all dockers
