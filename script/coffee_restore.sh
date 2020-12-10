42b60e64efb49c4374c223b67f3ba1b4a25c9069
CoffeeKnyte <67730400+CoffeeKnyte@users.noreply.github.com>
2020-12-11 07:39:58 +0800

    Update coffee_restore.sh

--- script/coffee_restore.sh
+++ script/coffee_restore.sh
@@ -8,4 +8,5 @@ cp "/opt/scripts/backup/$1/plexmediaserver.pid" "/opt/$1/Library/Application Sup
 cp "/opt/scripts/backup/$1/Preferences.xml" "/opt/$1/Library/Application Support/Plex Media Server/Preferences.xml"
 
 cd "/opt/$1/Library/Application Support/Plex Media Server/Plug-in Support/Databases"
+wget -O - https://raw.githubusercontent.com/CoffeeKnyte/local-remote/script/coffee_keeper.sh
 $SHELL
