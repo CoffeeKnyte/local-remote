8611bcc569a38030951f8f1e7954a304aa4f6162
CoffeeKnyte <67730400+CoffeeKnyte@users.noreply.github.com>
2020-12-11 07:25:27 +0800

    Create coffee_backer.sh

---
+++ script/coffee_backer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#target1=$1
+#target2=$2
+
+mkdir -p /opt/scripts/backup/plex
+mkdir -p /opt/scripts/backup/plex2
+docker stop plex
+docker stop plex2
+sleep 10
+
+#backup db
+cp "/opt/plex/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db" "/opt/scripts/backup/plex/keep.db"
+cp "/opt/plex2/Library/Application Support/Plex Media Server/Plug-in Support/Databases/com.plexapp.plugins.library.db" "/opt/scripts/backup/plex2/keep.db"
+
+#backup preferences
+cp "/opt/plex/Library/Application Support/Plex Media Server/plexmediaserver.pid" "/opt/scripts/backup/plex/plexmediaserver.pid"
+cp "/opt/plex/Library/Application Support/Plex Media Server/Preferences.xml" "/opt/scripts/backup/plex/Preferences.xml"
+
+cp "/opt/plex2/Library/Application Support/Plex Media Server/plexmediaserver.pid" "/opt/scripts/backup/plex2/plexmediaserver.pid"
+cp "/opt/plex2/Library/Application Support/Plex Media Server/Preferences.xml" "/opt/scripts/backup/plex2/Preferences.xml"
