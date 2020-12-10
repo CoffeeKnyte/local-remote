b5b8c2e007a591ac2dd2218fbdf09904dcd0402f
CoffeeKnyte <67730400+CoffeeKnyte@users.noreply.github.com>
2020-12-11 07:38:27 +0800

    Update coffee_restore.sh

--- script/coffee_restore.sh
+++ script/coffee_restore.sh
@@ -8,3 +8,4 @@ cp "/opt/scripts/backup/$1/plexmediaserver.pid" "/opt/$1/Library/Application Sup
 cp "/opt/scripts/backup/$1/Preferences.xml" "/opt/$1/Library/Application Support/Plex Media Server/Preferences.xml"
 
 cd "/opt/$1/Library/Application Support/Plex Media Server/Plug-in Support/Databases"
+$SHELL
