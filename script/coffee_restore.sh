dbd544abbc97dea524acdac573ede450359a0927
CoffeeKnyte <67730400+CoffeeKnyte@users.noreply.github.com>
2020-12-11 07:31:15 +0800

    Create coffee_restore.sh

---
+++ script/coffee_restore.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+target=$1
+
+#restore db
+cp "/opt/scripts/backup/$1/keep.db" "/opt/$1/Library/Application Support/Plex Media Server/Plug-in Support/Databases/keep.db"
+cp "/opt/scripts/backup/$1/plexmediaserver.pid" "/opt/$1/Library/Application Support/Plex Media Server/plexmediaserver.pid"
+cp "/opt/scripts/backup/$1/Preferences.xml" "/opt/$1/Library/Application Support/Plex Media Server/Preferences.xml"
+
+cd "/opt/$1/Library/Application Support/Plex Media Server/Plug-in Support/Databases"
