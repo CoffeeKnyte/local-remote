96f2d79cf3811d7d6543b442b1041b3e3f57710f
CoffeeKnyte <67730400+CoffeeKnyte@users.noreply.github.com>
2020-12-22 03:04:24 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,13 +3,14 @@
 mkdir -p /mnt/zenstorage
 mkdir -p /mnt/zenremote
 
+sudo rm /etc/systemd/system/zenunion.service
 sudo rm /etc/systemd/system/zenremote.service
 sudo rm /etc/systemd/system/zenremote_primer.service
 sudo rm /etc/systemd/system/zenremote_primer.timer
 sudo rm /etc/systemd/system/zenstorage.service
 sudo rm /etc/systemd/system/zenstorage_primer.service
 sudo rm /etc/systemd/system/zenstorage_primer.timer
-sudo rm  /etc/systemd/system/mergerfs.service
+sudo rm /etc/systemd/system/mergerfs.service
 
 sudo wget https://raw.githubusercontent.com/CoffeeKnyte/local-remote/main/service/zenremote.service -P /etc/systemd/system/
 sudo wget https://raw.githubusercontent.com/CoffeeKnyte/local-remote/main/service/zenremote_primer.service -P /etc/systemd/system/
