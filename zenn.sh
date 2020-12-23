9b1cc30b9684c0b25f6ea2bbcb31748a810c804f
CoffeeKnyte <67730400+CoffeeKnyte@users.noreply.github.com>
2020-12-24 05:03:06 +0800

    Update zenn.sh

--- zenn.sh
+++ zenn.sh
@@ -2,11 +2,11 @@
 set -x
 mkdir -p /mnt/unionfs
 
-#sudo systemctl disable mergerfs.service
-#sudo systemctl stop mergerfs.service
+sudo systemctl disable mergerfs.service
+sudo systemctl stop mergerfs.service
 
-#sudo systemctl disable zenstorage.service
-#sudo systemctl stop zenstorage.service
+sudo systemctl disable zenstorage.service
+sudo systemctl stop zenstorage.service
 #sudo systemctl disable zenstorage_primer.service
 #sudo systemctl stop zenstorage_primer.service
 #sudo systemctl disable zenstorage_primer.timer
@@ -22,10 +22,10 @@ mkdir -p /mnt/unionfs
 #sudo rm /etc/systemd/system/zenremote.service
 #sudo rm /etc/systemd/system/zenremote_primer.service
 #sudo rm /etc/systemd/system/zenremote_primer.timer
-#sudo rm /etc/systemd/system/zenstorage.service
+sudo rm /etc/systemd/system/zenstorage.service
 #sudo rm /etc/systemd/system/zenstorage_primer.service
 #sudo rm /etc/systemd/system/zenstorage_primer.timer
-#sudo rm /etc/systemd/system/mergerfs.service
+sudo rm /etc/systemd/system/mergerfs.service
 
 sudo rm /etc/systemd/system/zenunion.service
 sudo wget https://raw.githubusercontent.com/CoffeeKnyte/local-remote/main/service/zenunion.service -P /etc/systemd/system/
