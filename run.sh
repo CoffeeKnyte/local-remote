74379623dd46097c8218dc44ff872a226f775952
CoffeeKnyte <67730400+CoffeeKnyte@users.noreply.github.com>
2020-12-25 11:48:45 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,16 +4,35 @@ mkdir -p /mnt/zenstorage
 sudo touch /var/log/rclone-zenstorage.log
 sudo chown seed:seed /var/log/rclone-zenstorage.log
 
+sudo systemctl disable mergerfs.service
+sudo systemctl stop mergerfs.service
+
+sudo systemctl disable zenstorage.service
+sudo systemctl stop zenstorage.service
+sudo systemctl disable zenstorage_primer.service
+sudo systemctl stop zenstorage_primer.service
+sudo systemctl disable zenstorage_primer.timer
+sudo systemctl stop zenstorage_primer.timer
+
+sudo systemctl disable zenremote.service
+sudo systemctl stop zenremote.service
+sudo systemctl disable zenremote_primer.service
+sudo systemctl stop zenremote_primer.service
+sudo systemctl disable zenremote_primer.timer
+sudo systemctl stop zenremote_primer.timer
+
 sudo systemctl disable zenunion.service
 sudo systemctl stop zenunion.service
 sudo rm /etc/systemd/system/zenunion.service
-#sudo rm /etc/systemd/system/zenremote.service
-#sudo rm /etc/systemd/system/zenremote_primer.service
-#sudo rm /etc/systemd/system/zenremote_primer.timer
-#sudo rm /etc/systemd/system/zenstorage.service
-#sudo rm /etc/systemd/system/zenstorage_primer.service
-#sudo rm /etc/systemd/system/zenstorage_primer.timer
-#sudo rm /etc/systemd/system/mergerfs.service
+sudo rm /etc/systemd/system/zenremote.service
+sudo rm /etc/systemd/system/zenremote_primer.service
+sudo rm /etc/systemd/system/zenremote_primer.timer
+sudo rm /etc/systemd/system/zenstorage.service
+sudo rm /etc/systemd/system/zenstorage_primer.service
+sudo rm /etc/systemd/system/zenstorage_primer.timer
+sudo rm /etc/systemd/system/mergerfs.service
+
+
 
 sudo wget https://raw.githubusercontent.com/CoffeeKnyte/local-remote/main/service/zenstorage.service -P /etc/systemd/system/
 sudo wget https://raw.githubusercontent.com/CoffeeKnyte/local-remote/main/service/mergerfs.service -P /etc/systemd/system/
