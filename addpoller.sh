e7b25ac35c32af887fc24f2db145d930a94d064a
CoffeeKnyte <67730400+CoffeeKnyte@users.noreply.github.com>
2020-12-27 00:30:13 +0800

    Update addpoller.sh

--- addpoller.sh
+++ addpoller.sh
@@ -1,7 +1,6 @@
 sudo mv /opt/scripts/zenlocal/zenlocalpoller /opt/scripts/zenlocal/zenlocalpoller.old 
 sudo wget https://raw.githubusercontent.com/zenjabba/ZenDRIVE-Poller-Binary/main/zenlocalpoller -P /opt/scripts/zenlocal/
-sudo wget https://raw.githubusercontent.com/zenjabba/ZenDRIVE-Poller-Binary/main/start_poller.sh -P /opt/scripts/zenlocal/
-sudo mv start_poller.sh zenpoller.sh
+sudo wget https://raw.githubusercontent.com/CoffeeKnyte/local-remote/main/service/zenpoller.sh -P /opt/scripts/zenlocal/
 sudo chmod +x /opt/scripts/zenlocal/zen*
 sudo chown seed:seed /opt/scripts/zenlocal/zen*
 
