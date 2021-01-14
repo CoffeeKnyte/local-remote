613a22cbbca2f15e716962160b0e4bb56f596d7d
CoffeeKnyte <67730400+CoffeeKnyte@users.noreply.github.com>
2021-01-15 05:05:11 +0800

    Update warmup.sh

--- service/warmup.sh
+++ service/warmup.sh
@@ -9,7 +9,7 @@ find /mnt/zenstorage/zd-sports  -type d -maxdepth 3 &
 find /mnt/zenstorage/zd-tv1  -type d -maxdepth 3 &
 #find /mnt/zenstorage/zd-tv2  -type d -maxdepth 3 &
 find /mnt/zenstorage/zd-tv3  -type d -maxdepth 3 &
-find /mnt/zenstorage/zd-tv-non-english/tv_non-english/asian  -type d -maxdepth 2 &
-find /mnt/zenstorage/zd-inbound/inbound/coffee  -type d -maxdepth 2 &
+find /mnt/zenstorage/zd-tv-non-english/tv_non-english/asian  -type d -maxdepth 1 &
+find /mnt/zenstorage/zd-inbound/inbound/coffee  -type d -maxdepth 1 &
 # we need to wait till it's all done before we hand back control
 wait
