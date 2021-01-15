e8d054861529bcb62fba91f9364fb5ec8d7643d6
CoffeeKnyte <67730400+CoffeeKnyte@users.noreply.github.com>
2021-01-15 09:00:18 +0800

    Update warmup.sh

--- service/warmup.sh
+++ service/warmup.sh
@@ -7,7 +7,7 @@ find /mnt/zenstorage/zd-movies  -type d -maxdepth 3 &
 find /mnt/zenstorage/zd-movies-non-english/movies-non-english/Bollywood  -type d -maxdepth 1 &
 find /mnt/zenstorage/zd-sports  -type d -maxdepth 3 &
 find /mnt/zenstorage/zd-tv1  -type d -maxdepth 3 &
-#find /mnt/zenstorage/zd-tv2  -type d -maxdepth 3 &
+find /mnt/zenstorage/zd-tv2  -type d -maxdepth 3 &
 find /mnt/zenstorage/zd-tv3  -type d -maxdepth 3 &
 find /mnt/zenstorage/zd-tv-non-english/tv_non-english/asian  -type d -maxdepth 1 &
 find /mnt/zenstorage/zd-inbound/inbound/coffee  -type d -maxdepth 1 &
