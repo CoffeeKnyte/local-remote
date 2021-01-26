635973724030951feea10b387b47c7bd0504b8e0
CoffeeKnyte <67730400+CoffeeKnyte@users.noreply.github.com>
2021-01-26 12:13:39 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #bin/bash
 
 mkdir -p /opt/scripts/zenlocal
-sudo touch /var/log/rclone-zenstorage.log
-sudo touch /var/log/rclone-zenstorage-small.log
-sudo touch /var/log/rclone-zenstorage-metadata.log
-sudo chown seed:seed /var/log/rclone-*.log
+#sudo touch /var/log/rclone-zenstorage.log
+#sudo touch /var/log/rclone-zenstorage-small.log
+#sudo touch /var/log/rclone-zenstorage-metadata.log
+#sudo chown seed:seed /var/log/rclone-*.log
 
 kill $(pidof rclone)
 sudo umount /mnt/unionfs
@@ -18,6 +18,8 @@ sudo systemctl stop zenstorage-small.service
 sudo systemctl disable zenstorage-small.service
 sudo systemctl stop zenstorage-metadata.service
 sudo systemctl disable zenstorage-metadata.service
+sudo systemctl stop mergerfs.service
+sudo systemctl disable mergerfs.service
 
 sudo rm /etc/systemd/system/zenstorage.service
 sudo rm /etc/systemd/system/zenstorage-small.service
@@ -27,8 +29,8 @@ sudo rm /opt/scripts/zenlocal/warmup.sh
 
 
 sudo wget https://raw.githubusercontent.com/CoffeeKnyte/local-remote/main/service/zenstorage.service -P /etc/systemd/system/
-#sudo wget https://raw.githubusercontent.com/CoffeeKnyte/local-remote/main/service/zenstorage-small.service -P /etc/systemd/system/
-#sudo wget https://raw.githubusercontent.com/CoffeeKnyte/local-remote/main/service/zenstorage-metadata.service -P /etc/systemd/system/
+sudo wget https://raw.githubusercontent.com/CoffeeKnyte/local-remote/main/service/zenstorage-small.service -P /etc/systemd/system/
+sudo wget https://raw.githubusercontent.com/CoffeeKnyte/local-remote/main/service/zenstorage-metadata.service -P /etc/systemd/system/
 
 sudo wget https://raw.githubusercontent.com/CoffeeKnyte/local-remote/main/service/mergerfs.service -P /etc/systemd/system/
 sudo wget https://raw.githubusercontent.com/CoffeeKnyte/local-remote/main/service/warmup.sh -P /opt/scripts/zenlocal/
@@ -37,12 +39,13 @@ sudo chown seed:seed /opt/scripts/zenlocal/warmup.sh
 
 sudo systemctl daemon-reload
 sleep 1
+sudo systemctl enable zenstorage-small.service && sudo systemctl restart zenstorage-small.service
+sudo systemctl enable zenstorage-metadata.service && sudo systemctl restart zenstorage-metadata.service
 sudo systemctl enable zenstorage.service && sudo systemctl restart zenstorage.service
 echo "Finished priming"
-#sudo systemctl enable zenstorage-small.service && sudo systemctl restart zenstorage-small.service
-#sudo systemctl enable zenstorage-metadata.service && sudo systemctl restart zenstorage-metadata.service
 sudo rm -rf /mnt/unionfs/Media
 sudo systemctl enable mergerfs.service && sudo systemctl restart mergerfs.service 
 sleep 1
+sudo systemctl enable poller.service && sudo systemctl restart poller.service
 #restart all dockers
 docker restart $(docker ps -q)
