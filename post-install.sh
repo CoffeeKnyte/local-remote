ea9a03bf25c6eb4b7348a0043a75afc37e1d6961
CoffeeKnyte <67730400+CoffeeKnyte@users.noreply.github.com>
2021-05-25 02:28:35 +0000

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,3 +1,4 @@
+### Adding a 16GB swap.img file in btrfs
 sudo swapoff /swap.img
 sudo rm  /swap.img
 truncate -s 0 /swap.img
@@ -13,3 +14,12 @@ sudo nano /etc/fstab
 
 #check that the swap works
 #>> free -h
+
+
+### adding Specific rclone 1.54.0-beta-4948
+wget https://beta.rclone.org/v1.54.0-beta.4948.8a429d12c/rclone-v1.54.0-beta.4948.8a429d12c-linux-amd64.deb -O rclone-v1.54.0-beta-4948.deb 
+sudo dpkg -i ./rclone-v1.54.0-beta-4948.deb && rclone version
+### adding Specific mergerfs 2.30.0
+wget https://github.com/trapexit/mergerfs/releases/download/2.30.0/mergerfs_2.30.0.ubuntu-bionic_amd64.deb -P /opt/scripts/installers
+sudo dpkg -i  /opt/scripts/installers/mergerfs_2.30.0.ubuntu-bionic_amd64.deb
+
